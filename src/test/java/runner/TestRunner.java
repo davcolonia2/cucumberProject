1d7711e79385d0cadd5164c2fd9b5f68414d09c3
David Colonia <d.colonia@yahoo.com>
2024-06-08 10:15:02 -0700

    commit for review

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,5 +7,5 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",glue = "step_definitions",dryRun = false,tags = "@window",plugin = {"html:/target/reports/cucumber-reports.html"})
 public class TestRunner {
-
+//comment
 }
